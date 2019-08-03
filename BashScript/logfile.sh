338ab0f5ad8006e20b14cf5d2566a428f13c5ee0
Gurvinder <bhullar1313guri@gmail.com>
2019-08-02 22:02:39 -0400

    log file

---
+++ BashScript/logfile.sh
@@ -0,0 +1,10 @@
+Gur="/home/gurvinder/logfile/subfile.sh"
+while true; do
+today="$( date +"%s" )"
+number=0
+fname=$today.txt
+printf -v fname -- 'log.%s-%02d.txt' "$today" "$(( ++number ))"
+$Gur >> $fname
+sleep 10
+done
+
