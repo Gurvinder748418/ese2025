2196622806a8f16c3388212291e2c1e04fd532c5
Gurvinder <bhullar1313guri@gmail.com>
2019-08-02 21:59:14 -0400

    prof takis's code

---
+++ BashScript/advancelogfile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#check if a search has been specified
+numargs=$#
+if [ $numargs -lt 1 ]
+then
+     echo #new line separator
+     echo "Search string not specified"
+     exit
+fi
+
+#generate the log file 
+sstring=$1 #search string
+count=0 #counter variable
+LIMIT=10
+
+while [ "$count" -lt "$LIMIT" ]
+do
+datestr=`date +%s` #generate date string for file
+touch "log_$datestr.txt"
+uname >> log_$datestr.txt
+date >> log_$datestr.txt
+grep -m `expr $count + 1` $sstring /home/gurvinder/Downloads/words.txt | tail -1 >> log_$datestr.txt
+sleep 10 #wait for 10 seconds
+count=`expr $count + 1`
+done #while
+
+echo
+exit 0
+
+
