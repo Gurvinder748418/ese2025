c258925512d12686fac05e6f8ab4fc37555ea25b
Gurvinder <bhullar1313guri@gmail.com>
2019-08-14 08:32:18 -0400

    program to check file and word

---
+++ BashScript/checkfileword.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILE=$1
+WORD=$2
+NOWORD=0
+
+if [ -e "$FILE" ]
+then
+echo "File exist"
+else
+echo "File is not exist"
+fi
+#grep command used with wordcount 
+WORD_CONT=$(grep -i -o $WORD $FILE | wc -l)
+
+if [ "$WORD_CONT" -ne "$NOWORD" ]
+then
+echo "WORD FOUND "
+echo "The word "$WORD" appeard ="
+echo "$WORD_CONT"
+else
+echo "NO WORD FOUND"
+fi 
